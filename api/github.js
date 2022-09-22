381d6f5e29f780e887d442a91eafa507fce071d1
K.Kimura <dotnsf@gmail.com>
2022-09-22 17:48:40 +0900

    first commit

---
+++ api/github.js
@@ -0,0 +1,140 @@
+//. github.js
+
+var express = require( 'express' ),
+    bodyParser = require( 'body-parser' ),
+    request = require( 'request' ),
+    api = express();
+
+require( 'dotenv' ).config();
+
+var settings_cors = 'CORS' in process.env ? process.env.CORS : '';
+api.all( '/*', function( req, res, next ){
+  if( settings_cors ){
+    res.setHeader( 'Access-Control-Allow-Origin', settings_cors );
+    res.setHeader( 'Vary', 'Origin' );
+  }
+  next();
+});
+
+api.use( bodyParser.urlencoded( { extended: true } ) );
+api.use( bodyParser.json() );
+api.use( express.Router() );
+
+//. Issues
+api.getIssues = async function( user, repo, params_obj, token ){
+  return new Promise( async function( resolve, reject ){
+    var url = 'https://api.github.com/repos/' + user + '/' + repo + '/issues';
+    if( params_obj && typeof params_obj == 'object' ){
+      var params = [];
+      Object.keys( params_obj ).forEach( function( key ){
+        var value = params_obj[key];
+        params.push( key + '=' + value );
+      });
+      if( params.length > 0 ){
+        url += ( '?' + params.join( '&' ) );
+      }
+    }
+
+    var options = {
+      url: url,
+      method: 'GET',
+      headers: { Accept: 'application/json', 'User-Agent': 'github-issues-api' }
+    };
+    if( token ){
+      options.headers.Authorization = 'token ' + token;
+    }
+    request( options, ( err, res, body ) => {
+      if( err ){
+        console.log( { err } );
+        resolve( { status: false, error: err } );
+      }else{
+        if( typeof body == 'string' ){
+          body = JSON.parse( body );
+        }
+        //console.log( { body } );
+        resolve( { status: true, params: params_obj, issues: body } );
+      }
+    });
+  });
+};
+
+//. Comments
+api.getComments = async function( user, repo, params_obj, token ){
+  return new Promise( async function( resolve, reject ){
+    var url = 'https://api.github.com/repos/' + user + '/' + repo + '/issues/comments';
+    if( params_obj && typeof params_obj == 'object' ){
+      var params = [];
+      Object.keys( params_obj ).forEach( function( key ){
+        var value = params_obj[key];
+        params.push( key + '=' + value );
+      });
+      if( params.length > 0 ){
+        url += ( '?' + params.join( '&' ) );
+      }
+    }
+
+    var options = {
+      url: url,
+      method: 'GET',
+      headers: { Accept: 'application/json', 'User-Agent': 'github-issues-api' }
+    };
+    if( token ){
+      options.headers.Authorization = 'token ' + token;
+    }
+    request( options, ( err, res, body ) => {
+      if( err ){
+        console.log( { err } );
+        resolve( { status: false, error: err } );
+      }else{
+        if( typeof body == 'string' ){
+          body = JSON.parse( body );
+        }
+        //console.log( { body } );
+        resolve( { status: true, params: params_obj, comments: body } );
+      }
+    });
+  });
+};
+
+
+api.get( '/issues/:user/:repo', async function( req, res ){
+  res.contentType( 'application/json; charset=utf-8' );
+
+  var user = req.params.user;
+  var repo = req.params.repo;
+  if( user && repo ){
+    var token = req.query.token;
+    api.getIssues( user, repo, null, token ).then( function( result ){
+      res.status( result.status ? 200 : 400 );
+      res.write( JSON.stringify( result, null, 2 ) );
+      res.end();
+    });
+  }else{
+    res.status( 400 );
+    res.write( JSON.stringify( { status: false, error: 'parameter user & repo are mandatory.' }, null, 2 ) );
+    res.end();
+  }
+});
+
+api.get( '/comments/:user/:repo', async function( req, res ){
+  res.contentType( 'application/json; charset=utf-8' );
+
+  var user = req.params.user;
+  var repo = req.params.repo;
+  if( user && repo ){
+    var token = req.query.token;
+    api.getComments( user, repo, null, token ).then( function( result ){
+      res.status( result.status ? 200 : 400 );
+      res.write( JSON.stringify( result, null, 2 ) );
+      res.end();
+    });
+  }else{
+    res.status( 400 );
+    res.write( JSON.stringify( { status: false, error: 'parameter user & repo are mandatory.' }, null, 2 ) );
+    res.end();
+  }
+});
+
+
+//. api をエクスポート
+module.exports = api;
---
+++ public/main.js
@@ -0,0 +1,57 @@
+//. main.js
+$(function(){
+  $('#github_user').keyup( function( e ){
+    showUserRepo();
+  });
+  $('#github_repo').keyup( function( e ){
+    showUserRepo();
+  });
+});
+
+function showUserRepo(){
+  var user = $('#github_user').val();
+  var repo = $('#github_repo').val();
+  var user_repo = user + '/' + repo;
+  $('#user_repo').html( user_repo );
+}
+
+function getIssues(){
+  $('#debug').html( '' );
+  var user = $('#github_user').val();
+  var repo = $('#github_repo').val();
+  if( user && repo ){
+    var token = $('#github_token').val();
+    $.ajax({
+      type: 'GET',
+      url: API_SERVER + '/api/github/issues/' + user + '/' + repo + '?token=' + token,
+      success: function( result ){
+        $('#debug').css( 'color', '#008' );
+        $('#debug').html( JSON.stringify( result.issues, null, 2 ) );
+      },
+      error: function( e0, e1, e2 ){
+        console.log( e0, e1, e2 );
+      }
+    });
+  }
+}
+
+function getComments(){
+  $('#debug').html( '' );
+  var user = $('#github_user').val();
+  var repo = $('#github_repo').val();
+  if( user && repo ){
+    var token = $('#github_token').val();
+    $.ajax({
+      type: 'GET',
+      url: API_SERVER + '/api/github/comments/' + user + '/' + repo + '?token=' + token,
+      success: function( result ){
+        $('#debug').css( 'color', '#080' );
+        $('#debug').html( JSON.stringify( result.comments, null, 2 ) );
+      },
+      error: function( e0, e1, e2 ){
+        console.log( e0, e1, e2 );
+      }
+    });
+  }
+}
+
