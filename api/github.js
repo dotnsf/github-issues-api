17d4605cee71c58bcabe213bab64d9ce5f59f785
K.Kimura <dotnsf@gmail.com>
2022-10-02 19:00:21 +0900

    UI fix for github-issues-api

--- api/github.js
+++ api/github.js
@@ -101,6 +101,120 @@ api.getComments = async function( user, repo, issue_num, params_obj, token ){
   });
 };
 
+//. Assignees
+api.getAssignees = async function( user, repo, params_obj, token ){
+  return new Promise( async function( resolve, reject ){
+    var url = 'https://api.github.com/repos/' + user + '/' + repo + '/assignees';
+    if( params_obj && typeof params_obj == 'object' ){
+      var params = [];
+      Object.keys( params_obj ).forEach( function( key ){
+        var value = params_obj[key];
+        params.push( key + '=' + value );
+      });
+      if( params.length > 0 ){
+        url += ( '?' + params.join( '&' ) );
+      }
+    }
+
+    var options = {
+      url: url,
+      method: 'GET',
+      headers: { Accept: 'application/json', 'User-Agent': 'github-issues-api' }
+    };
+    if( token ){
+      options.headers.Authorization = 'token ' + token;
+    }
+    request( options, ( err, res, body ) => {
+      if( err ){
+        console.log( { err } );
+        resolve( { status: false, error: err } );
+      }else{
+        if( typeof body == 'string' ){
+          body = JSON.parse( body );
+        }
+        //console.log( { body } );
+        resolve( { status: true, params: params_obj, res_headers: res.headers, assignees: body } );
+      }
+    });
+  });
+};
+
+//. Labels
+api.getLabels = async function( user, repo, params_obj, token ){
+  return new Promise( async function( resolve, reject ){
+    var url = 'https://api.github.com/repos/' + user + '/' + repo + '/labels';
+    if( params_obj && typeof params_obj == 'object' ){
+      var params = [];
+      Object.keys( params_obj ).forEach( function( key ){
+        var value = params_obj[key];
+        params.push( key + '=' + value );
+      });
+      if( params.length > 0 ){
+        url += ( '?' + params.join( '&' ) );
+      }
+    }
+
+    var options = {
+      url: url,
+      method: 'GET',
+      headers: { Accept: 'application/json', 'User-Agent': 'github-issues-api' }
+    };
+    if( token ){
+      options.headers.Authorization = 'token ' + token;
+    }
+    request( options, ( err, res, body ) => {
+      if( err ){
+        console.log( { err } );
+        resolve( { status: false, error: err } );
+      }else{
+        if( typeof body == 'string' ){
+          body = JSON.parse( body );
+        }
+        //console.log( { body } );
+        resolve( { status: true, params: params_obj, res_headers: res.headers, labels: body } );
+      }
+    });
+  });
+};
+
+//. Milestones
+api.getMilestones = async function( user, repo, params_obj, token ){
+  return new Promise( async function( resolve, reject ){
+    var url = 'https://api.github.com/repos/' + user + '/' + repo + '/milestones';
+    if( params_obj && typeof params_obj == 'object' ){
+      var params = [];
+      Object.keys( params_obj ).forEach( function( key ){
+        var value = params_obj[key];
+        params.push( key + '=' + value );
+      });
+      if( params.length > 0 ){
+        url += ( '?' + params.join( '&' ) );
+      }
+    }
+
+    var options = {
+      url: url,
+      method: 'GET',
+      headers: { Accept: 'application/json', 'User-Agent': 'github-issues-api' }
+    };
+    if( token ){
+      options.headers.Authorization = 'token ' + token;
+    }
+    request( options, ( err, res, body ) => {
+      if( err ){
+        console.log( { err } );
+        resolve( { status: false, error: err } );
+      }else{
+        if( typeof body == 'string' ){
+          body = JSON.parse( body );
+        }
+        //console.log( { body } );
+        resolve( { status: true, params: params_obj, res_headers: res.headers, milestones: body } );
+      }
+    });
+  });
+};
+
 
 api.get( '/issues/:user/:repo', async function( req, res ){
   res.contentType( 'application/json; charset=utf-8' );
@@ -183,6 +297,138 @@ api.get( '/comments/:user/:repo', async function( req, res ){
     }
 
     res.write( JSON.stringify( { status: true, headers: result.res_headers, comments: comments, api_call: page }, null, 2 ) );
+    res.end();
+  }else{
+    res.status( 400 );
+    res.write( JSON.stringify( { status: false, error: 'parameter user & repo are mandatory.' }, null, 2 ) );
+    res.end();
+  }
+});
+
+api.get( '/assignees/:user/:repo', async function( req, res ){
+  res.contentType( 'application/json; charset=utf-8' );
+
+  var user = req.params.user;
+  var repo = req.params.repo;
+  if( user && repo ){
+    var token = req.query.token;
+
+    var params_obj = { per_page: PER_PAGE };
+    Object.keys( req.query ).forEach( function( key ){
+      /*
+      if( [ 'filter', 'state', 'labels' ].indexOf( key ) > -1 ){
+        params_obj[key] = req.query[key];
+      }
+      */
+    });
+
+    var assignees = [];
+    var page = 1;
+    var b = true;
+    var result = null;
+    while( b ){
+      params_obj['page'] = page;
+      result = await api.getAssignees( user, repo, params_obj, token );
+      if( result.status && result.assignees && result.assignees.length > 0 ){
+        assignees = assignees.concat( result.assignees );
+      }
+
+      if( !result.assignees || result.assignees.length < PER_PAGE ){
+        b = false;
+      }else{
+        page ++;
+      }
+    }
+
+    res.write( JSON.stringify( { status: true, headers: result.res_headers, assignees: assignees, api_call: page }, null, 2 ) );
+    res.end();
+  }else{
+    res.status( 400 );
+    res.write( JSON.stringify( { status: false, error: 'parameter user & repo are mandatory.' }, null, 2 ) );
+    res.end();
+  }
+});
+
+api.get( '/labels/:user/:repo', async function( req, res ){
+  res.contentType( 'application/json; charset=utf-8' );
+
+  var user = req.params.user;
+  var repo = req.params.repo;
+  if( user && repo ){
+    var token = req.query.token;
+
+    var params_obj = { per_page: PER_PAGE };
+    Object.keys( req.query ).forEach( function( key ){
+      /*
+      if( [ 'filter', 'state', 'labels' ].indexOf( key ) > -1 ){
+        params_obj[key] = req.query[key];
+      }
+      */
+    });
+
+    var labels = [];
+    var page = 1;
+    var b = true;
+    var result = null;
+    while( b ){
+      params_obj['page'] = page;
+      result = await api.getLabels( user, repo, params_obj, token );
+      if( result.status && result.labels && result.labels.length > 0 ){
+        labels = labels.concat( result.labels );
+      }
+
+      if( !result.labels || result.labels.length < PER_PAGE ){
+        b = false;
+      }else{
+        page ++;
+      }
+    }
+
+    res.write( JSON.stringify( { status: true, headers: result.res_headers, labels: labels, api_call: page }, null, 2 ) );
+    res.end();
+  }else{
+    res.status( 400 );
+    res.write( JSON.stringify( { status: false, error: 'parameter user & repo are mandatory.' }, null, 2 ) );
+    res.end();
+  }
+});
+
+api.get( '/milestones/:user/:repo', async function( req, res ){
+  res.contentType( 'application/json; charset=utf-8' );
+
+  var user = req.params.user;
+  var repo = req.params.repo;
+  if( user && repo ){
+    var token = req.query.token;
+
+    var params_obj = { per_page: PER_PAGE };
+    Object.keys( req.query ).forEach( function( key ){
+      /*
+      if( [ 'filter', 'state', 'labels' ].indexOf( key ) > -1 ){
+        params_obj[key] = req.query[key];
+      }
+      */
+    });
+
+    var milestones = [];
+    var page = 1;
+    var b = true;
+    var result = null;
+    while( b ){
+      params_obj['page'] = page;
+      result = await api.getMilestones( user, repo, params_obj, token );
+      if( result.status && result.milestones && result.milestones.length > 0 ){
+        milestones = milestones.concat( result.milestones );
+      }
+
+      if( !result.milestones || result.milestones.length < PER_PAGE ){
+        b = false;
+      }else{
+        page ++;
+      }
+    }
+
+    res.write( JSON.stringify( { status: true, headers: result.res_headers, milestones: milestones, api_call: page }, null, 2 ) );
     res.end();
   }else{
     res.status( 400 );
--- public/main.js
+++ public/main.js
@@ -6,6 +6,16 @@ $(function(){
   $('#github_repo').keyup( function( e ){
     showUserRepo();
   });
+  $('#github_issue_num').keyup( function( e ){
+    var issue_num = $('#github_issue_num').val();
+    if( issue_num ){
+      $('.mybtn').prop( 'disabled', true );
+      $('#mybtn-comments').prop( 'disabled', false );
+    }else{
+      $('.mybtn').prop( 'disabled', false );
+      $('#mybtn-comments').prop( 'disabled', true );
+    }
+  });
 });
 
 function showUserRepo(){
@@ -26,6 +36,7 @@ function getIssues(){
       url: API_SERVER + '/api/github/issues/' + user + '/' + repo + '?token=' + token,
       success: function( result ){
         console.log( result );
+        showRateLimitReset( result );
         $('#debug').css( 'color', '#008' );
         $('#debug').html( JSON.stringify( result.issues, null, 2 ) );
       },
@@ -48,6 +59,7 @@ function getComments(){
       url: API_SERVER + '/api/github/comments/' + user + '/' + repo + '?token=' + token + '&issue_num=' + issue_num,
       success: function( result ){
         console.log( result );
+        showRateLimitReset( result );
         $('#debug').css( 'color', '#080' );
         $('#debug').html( JSON.stringify( result.comments, null, 2 ) );
       },
@@ -55,6 +67,97 @@ function getComments(){
         console.log( e0, e1, e2 );
       }
     });
+  }
+}
+
+function getAssignees(){
+  $('#debug').html( '' );
+  var user = $('#github_user').val();
+  var repo = $('#github_repo').val();
+  if( user && repo ){
+    var token = $('#github_token').val();
+    $.ajax({
+      type: 'GET',
+      url: API_SERVER + '/api/github/assignees/' + user + '/' + repo + '?token=' + token,
+      success: function( result ){
+        console.log( result );
+        showRateLimitReset( result );
+        $('#debug').css( 'color', '#088' );
+        $('#debug').html( JSON.stringify( result.assignees, null, 2 ) );
+      },
+      error: function( e0, e1, e2 ){
+        console.log( e0, e1, e2 );
+      }
+    });
+  }
+}
+
+function getLabels(){
+  $('#debug').html( '' );
+  var user = $('#github_user').val();
+  var repo = $('#github_repo').val();
+  if( user && repo ){
+    var token = $('#github_token').val();
+    $.ajax({
+      type: 'GET',
+      url: API_SERVER + '/api/github/labels/' + user + '/' + repo + '?token=' + token,
+      success: function( result ){
+        console.log( result );
+        showRateLimitReset( result );
+        $('#debug').css( 'color', '#880' );
+        $('#debug').html( JSON.stringify( result.labels, null, 2 ) );
+      },
+      error: function( e0, e1, e2 ){
+        console.log( e0, e1, e2 );
+      }
+    });
+  }
+}
+
+function getMilestones(){
+  $('#debug').html( '' );
+  var user = $('#github_user').val();
+  var repo = $('#github_repo').val();
+  if( user && repo ){
+    var token = $('#github_token').val();
+    $.ajax({
+      type: 'GET',
+      url: API_SERVER + '/api/github/milestones/' + user + '/' + repo + '?token=' + token,
+      success: function( result ){
+        console.log( result );
+        showRateLimitReset( result );
+        $('#debug').css( 'color', '#800' );
+        $('#debug').html( JSON.stringify( result.milestones, null, 2 ) );
+      },
+      error: function( e0, e1, e2 ){
+        console.log( e0, e1, e2 );
+      }
+    });
+  }
+}
+
+function showRateLimitReset( result ){
+  if( result && result.headers && result.headers['x-ratelimit-reset'] ){
+    var remain = result.headers['x-ratelimit-remaining'];
+    var reset = result.headers['x-ratelimit-reset'];
+    if( typeof reset == 'string' ){ reset = parseInt( reset ); }
+    reset *= 1000;
+    var t = new Date( reset );
+
+    var y = t.getFullYear();
+    var m = t.getMonth() + 1;
+    var d = t.getDate();
+    var h = t.getHours();
+    var n = t.getMinutes();
+    var s = t.getSeconds();
+
+    var ymdhns = y
+      + '-' + ( m < 10 ? '0' : '' ) + m
+      + '-' + ( d < 10 ? '0' : '' ) + d
+      + ' ' + ( h < 10 ? '0' : '' ) + h
+      + ':' + ( n < 10 ? '0' : '' ) + n
+      + ':' + ( s < 10 ? '0' : '' ) + s;
+    console.log( 'next reset : ' + ymdhns + ' remain(' + remain + ')' );
   }
 }
 
